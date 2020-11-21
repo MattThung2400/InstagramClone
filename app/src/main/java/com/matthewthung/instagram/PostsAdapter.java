d0b0449bc28251f383d29e9144f2e9a99204fb42
Matthew Thung <matthewgh2400@gmail.com>
2020-11-21 18:04:08 -0500

    Removed unused implementations in the build.grade file.

--- app/src/main/java/com/matthewthung/instagram/PostsAdapter.java
+++ app/src/main/java/com/matthewthung/instagram/PostsAdapter.java
@@ -13,7 +13,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.parse.GetDataCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
